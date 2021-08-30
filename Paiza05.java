a4192fb5f3dc643ff52936734c92aa429e847b0b
Yumiko <wakinghira73@gmail.com>
2021-08-30 11:46:00 +0900

    first commit

---
+++ Paiza05.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Paiza05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println(number);
+        
+        if (number == 10) {
+            System.out.println("Welcome");
+        
+        } else {
+            System.out.println("Goodbye");
+        }
+        
+    }
+}
+
---
+++ Paiza06.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Paiza06 {
+   public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int number = scan.nextInt();
+       System.out.println(number);
+
+       if (number == 10) {
+           System.out.println("Welcom");
+       } else if (number > 10) {
+           System.out.println("Good morning");
+       } else {
+           System.out.println("Goodbye");
+       }
+    } 
+
+}
+
+
+
+
+
+
+
+
+
+     
